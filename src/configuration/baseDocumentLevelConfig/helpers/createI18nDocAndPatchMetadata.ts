96c06758caad9e43cc1258202c610c8d74bfe024
cngonzalez <carolina.nicole.gonzalez@gmail.com>
2023-09-05 14:00:09 -0700

    fix: imports should be drafts, and no key dupes on translation metadata (#28)

--- src/configuration/baseDocumentLevelConfig/helpers/createI18nDocAndPatchMetadata.ts
+++ src/configuration/baseDocumentLevelConfig/helpers/createI18nDocAndPatchMetadata.ts
@@ -4,21 +4,32 @@ export const createI18nDocAndPatchMetadata = (
   translatedDoc: SanityDocumentLike,
   localeId: string,
   client: SanityClient,
-  translationMetadataId: string,
+  translationMetadata: SanityDocumentLike,
 ): void => {
   translatedDoc.language = localeId
+  const translations = translationMetadata.translations as Record<string, any>[]
+  const existingLocaleKey = translations.find((translation) => translation._key === localeId)
+  const operation = existingLocaleKey ? 'replace' : 'after'
+  const location = existingLocaleKey ? `translations[_key == "${localeId}"]` : 'translations[-1]'
+
   //remove system fields
-  const {_id, _rev, _updatedAt, _createdAt, ...rest} = translatedDoc
-  client.create(rest).then((doc) => {
+  const {_updatedAt, _createdAt, ...rest} = translatedDoc
+  client.create({...rest, _id: 'drafts.'}).then((doc) => {
+    const _ref = doc._id.replace('drafts.', '')
     client
       .transaction()
-      .patch(translationMetadataId, (p) =>
-        p.insert('after', 'translations[-1]', [
+      .patch(translationMetadata._id, (p) =>
+        p.insert(operation, location, [
           {
             _key: localeId,
             value: {
               _type: 'reference',
-              _ref: doc._id,
+              _ref,
+              _weak: true,
+              _strengthenOnPublish: {
+                _weak: false,
+                type: doc._type,
+              },
             },
           },
         ]),
